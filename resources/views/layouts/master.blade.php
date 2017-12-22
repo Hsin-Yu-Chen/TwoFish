c0de26eec9ee7a9455d2b718d60ab9536ad0c62c
陳欣妤 <s3a432058@students.ncut.edu.tw>
2017-12-22 16:06:17 +0800

    修改主樣板內link、script標籤css、js檔案的參考位置，利用asset()輔助方法

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,10 +11,10 @@
     <title>Modern Business - Start Bootstrap Template</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="{{asset('css/modern-business.css')}}" rel="stylesheet">
 
 </head>
 
@@ -29,8 +29,8 @@
 @include('layouts.partials.footer')
 
 <!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('jquery/jquery.min.js')}}vendor/"></script>
+<script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
 </body>
 
